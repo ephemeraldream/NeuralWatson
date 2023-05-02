4bfb0e2fa0aaa2e6a06e3723e84efa06d91cffd7
XD <bladee.tr@yandex.ru>
2023-05-02 03:09:03 -0700

    naive indexation is completed. Working on Neural

---
+++ demo/tom/src/main/java/SynonymParser.java
@@ -0,0 +1,50 @@
+
+import java.io.*;
+import java.util.*;
+
+import org.json.simple.parser.ParseException;
+
+public class SynonymParser {
+
+    public static void main(String[] args) throws IOException, ParseException {
+        HashMap<String, String[]> synonyms = new HashMap<>();
+        Scanner scanner = new Scanner(new File("another_data/synonyms.csv"));
+        scanner.useDelimiter(",");
+        boolean trigger = false;
+        while (scanner.hasNext()){
+            if (!trigger){
+                trigger = true;
+                scanner.nextLine();
+            }
+            String toParse = scanner.nextLine();
+            List<String> splitted = new LinkedList<String>(List.of(toParse.split(",")));
+            splitted.remove(1);
+            synonyms.put(splitted.get(0), splitted.get(1).split(";"));
+
+            int iasdf = 32;
+
+
+
+        }
+                try {
+            FileOutputStream myFileOutStream
+                = new FileOutputStream(
+                    "another_data/parsedSynonyms");
+
+            ObjectOutputStream myObjectOutStream
+                = new ObjectOutputStream(myFileOutStream);
+
+            myObjectOutStream.writeObject(synonyms);
+
+            // closing FileOutputStream and
+            // ObjectOutputStream
+            myObjectOutStream.close();
+            myFileOutStream.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    }
+
